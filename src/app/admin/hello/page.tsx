aa951f889197d575dc923bf03506eb740d63002e
PC2Home <indiabeeps@gmail.com>
2024-12-01 17:52:37 +0530

    fresh

--- src/app/admin/hello/page.tsx
+++ src/app/admin/hello/page.tsx
@@ -1,9 +1,11 @@
 import Counter from '../../components/Counter';
 
-// done 2nd commit
+// done 3nd commit
 export default function Page() {
     return (
         <div className='h-screen flex items-center justify-center bg-gray-500'>
+            <Counter />
+            <Counter />
             <Counter />
         </div>
     );
