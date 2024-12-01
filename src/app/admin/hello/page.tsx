823b0b1f6e70fd0dd4a86ef2c11f39fa35f42bd2
PC2Home <indiabeeps@gmail.com>
2024-12-01 17:49:03 +0530

    removed virus

--- src/app/admin/hello/page.tsx
+++ src/app/admin/hello/page.tsx
@@ -1,12 +1,9 @@
 import Counter from '../../components/Counter';
 
+// done 2nd commit
 export default function Page() {
     return (
         <div className='h-screen flex items-center justify-center bg-gray-500'>
-            <Counter />
-            <Counter />
-            <Counter />
-            <Counter />
             <Counter />
         </div>
     );
